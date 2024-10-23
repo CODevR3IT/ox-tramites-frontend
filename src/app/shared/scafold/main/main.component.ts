a5cb66a1f136635c2eeaccaa73f6e67074a34550
Redd <redd_gard@icloud.com>
2024-10-23 13:43:05 -0600

    style scss

--- src/app/shared/scafold/main/main.component.ts
+++ src/app/shared/scafold/main/main.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-main',
@@ -10,6 +11,7 @@ import { HeaderComponent } from '../header/header.component';
   imports: [
     RouterOutlet,
     HeaderComponent,
+    MenuComponent,
     FooterComponent,
   ],
   templateUrl: './main.component.html',
