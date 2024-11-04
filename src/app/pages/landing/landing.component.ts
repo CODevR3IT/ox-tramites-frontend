6c8162cc4c344220a588d1d03452d2df4fed7183
Redd <redd_gard@icloud.com>
2024-11-04 12:14:33 -0600

    animations

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,12 +1,14 @@
+import { transition, trigger, useAnimation } from '@angular/animations';
 import { Component } from '@angular/core';
-
+import { fadeInLeft } from 'ng-animate';
 @Component({
   selector: 'app-landing',
   standalone: true,
   imports: [],
   templateUrl: './landing.component.html',
-  styles: ``
+  styles: ``,
+  animations: [trigger('fadeInLeft', [transition('* => *', useAnimation(fadeInLeft))])],
 })
 export class LandingComponent {
-
+  fadeInLeft:any
 }
