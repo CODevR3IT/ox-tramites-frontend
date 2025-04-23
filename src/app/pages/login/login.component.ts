ae3d00c402c5bba71622edd01ec6d5f242fab519
iovidal33 <iovidal33@gmail.com>
2025-04-23 00:39:24 -0600

    LOGIN SERVICIOS 2 V1.4

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,18 @@
 import { transition, trigger, useAnimation } from '@angular/animations';
-import { Component, ElementRef, OnInit, ViewChild, HostListener } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, HostListener, inject } from '@angular/core';
 import { fadeInLeft } from 'ng-animate';
 import { CommonModule } from '@angular/common';
-import { FormGroup, FormControl, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { isValidControl, getMessageError } from '../../shared/forms/input-validator';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule, FormsModule, FormBuilder } from '@angular/forms';
+import { AuthService } from '../../shared/auth/auth.service';
 import { RegistroService } from '../../shared/services/registro.service';
 import Swal from 'sweetalert2';
 import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   animations: [
@@ -17,36 +20,47 @@ import { NgxSpinnerService } from 'ngx-spinner';
   ],
 })
 export class LoginComponent {
+  private formBuilder = inject(FormBuilder);
   payload:any = {
     correo: '',
     password: ''
   };
+  loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(
+          /(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/
+        ),
+      ],
+    ],
+  });
+
   fadeInLeft: any;
   isSmallScreen: boolean = false;
   showPassword: boolean = false;
-  valida: boolean = false;
-  correoError: string = '';
-  cambioPassword: boolean = true;
 
   constructor(
+    private readonly route: ActivatedRoute,
+    private readonly router: Router,
+    private readonly authService: AuthService,
     private spinner: NgxSpinnerService,
   ){}
 
-  validaForm(): any{
-    this.correoError = '';
-    if (!this.isValidEmail(this.payload.correo)) { 
-      this.correoError = 'Correo inválido. Usa el formato: nombre@dominio.com';
-      return false;
-    }
-    // if(this.existeCodigo){
-    //   console.log("aca entre2");
-    //   return this.valida = (this.payload.correo !== '' && this.payload.password !== '' && this.payload.codigo !== '' ? true : false);
-    // }else{
-    //   console.log("aca entre3");
-    //   return this.valida = (this.payload.correo !== '' && this.payload.password !== '' ? true : false);
-    // }
+  get emailErrors() {
+    return isValidControl(this.loginForm.get('email'));
   }
-
+  get emailMessageError(){
+    return getMessageError(this.loginForm.get('email')?.errors, 'correo electrónico');
+  }
+  get passwordErrors() {
+    return isValidControl(this.loginForm.get('password'));
+  }
+  get passwordMessageError(){
+    return getMessageError(this.loginForm.get('password')?.errors, 'contraseña', 'La contraseña debe de tener mínimo 6 caracteres con una letra mayúscula, minúscula y un número.');
+  }
 
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
@@ -56,25 +70,12 @@ export class LoginComponent {
     this.isSmallScreen = window.innerWidth < 768; // <768px se considera pantalla chica
   }
 
+  onSubmit() {
+    console.log("aca3521")
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
 
-  isValidEmail(email: string): boolean {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email.trim());
-  }
-
-
-  validaFormPass(): any{
-    if (!this.isValidEmail(this.payload.correo)) { 
-      this.correoError = 'Correo inválido. Usa el formato: nombre@dominio.com';
-      return false
+      this.authService.login(email!, password!).subscribe();
     }
-    if((this.payload.cambiaPassword !== this.payload.cambiaPasswordB) 
-        || (this.payload.cambiaPassword.trim() === '') 
-        || (this.payload.cambiaPasswordB.trim() === undefined)){ 
-          return false;
-        }else{
-          return true;
-        }
   }
-
 }
