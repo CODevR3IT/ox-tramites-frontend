ac87408fce749e4abc1634b9ed97e7377d0bdd74
Redd <redd_gard@icloud.com>
2025-07-15 15:11:10 -0600

    filtro trámites

--- src/app/pages/mis-tramites/mis-tramites.component.ts
+++ src/app/pages/mis-tramites/mis-tramites.component.ts
@@ -5,22 +5,42 @@ import { RouterLink } from '@angular/router';
 import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 import { environment } from '../../../environments/environment';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-mis-tramites',
   standalone: true,
-  imports: [RouterLink, NgbAccordionModule, CommonModule],
+  imports: [RouterLink, NgbAccordionModule, CommonModule, FormsModule],
   templateUrl: './mis-tramites.component.html',
   styles: ``
 })
 export class MisTramitesComponent {
   @Input() categorias: Categoria[] = [];
   fileEndpoint = environment.fileEndpoint + '/files/';
+    filterText = '';
   constructor(
   ) { }
 
   ngOnInit(): void {
     
+  }
+  get categoriasFiltradas(): Categoria[] {
+     const quitarAcentos = (texto: string) =>
+    texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+
+  if (!this.filterText.trim()) {
+    return this.categorias;
+  }
+  const filtro = quitarAcentos(this.filterText);
+
+  return this.categorias
+    .map(cat => ({
+      ...cat,
+      tramites: cat.tramites.filter(tramite =>
+        quitarAcentos(tramite.tramite).includes(filtro)
+      )
+    }))
+    .filter(cat => cat.tramites.length > 0);
   }
 
 }
--- src/app/pages/tramites-servicios/tramites-servicios.component.ts
+++ src/app/pages/tramites-servicios/tramites-servicios.component.ts
@@ -4,20 +4,40 @@ import { Categoria } from '../mis-tramites/interfaces/tramites.interface';
 import { MisTramitesService } from '../mis-tramites/mis-tramites.service';
 import { environment } from '../../../environments/environment';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tramites-servicios',
-  imports: [NgbAccordionModule,RouterLink],
+  imports: [NgbAccordionModule,RouterLink, FormsModule],
   templateUrl: './tramites-servicios.component.html',
   styles: ``
 })
 export class TramitesServiciosComponent {
   @Input() categorias: Categoria[] = [];
   fileEndpoint = environment.fileEndpoint + '/files/';
+  filterText = '';
   constructor(
     private readonly misTramitesService: MisTramitesService,
   ) { }
 
   ngOnInit(): void {
+  }
+    get categoriasFiltradas(): Categoria[] {
+     const quitarAcentos = (texto: string) =>
+    texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+
+  if (!this.filterText.trim()) {
+    return this.categorias;
+  }
+  const filtro = quitarAcentos(this.filterText);
+
+  return this.categorias
+    .map(cat => ({
+      ...cat,
+      tramites: cat.tramites.filter(tramite =>
+        quitarAcentos(tramite.tramite).includes(filtro)
+      )
+    }))
+    .filter(cat => cat.tramites.length > 0);
   }
 }
